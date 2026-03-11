83a8fbf868af80e1e0b9210608aa002c76c469bb
Heber Robert <euheber1@gmail.com>
2026-03-11 20:56:53 -0300

    fix: tsconfig moduleResolution

--- src/http/controllers/create-product.ts
+++ src/http/controllers/create-product.ts
@@ -1,8 +1,9 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "../../app";
+import type {ProductsCreateInput} from "../../../generated/prisma/models"
+
 
 export async function createProducts(request: FastifyRequest, reply: FastifyReply){
-
-
     
     return reply.send("Tudo ok").status(200)
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { createProducts } from "./controllers/create-product";
 
 export async function routes(app: FastifyInstance){
-
+    
     app.get("/home", createProducts)
 
 }
